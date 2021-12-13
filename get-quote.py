2ee926f419bcabad1cc124dcb3c8ce99f04c9532
Sourcery AI <>
2021-12-13 08:19:39 +0000

    'Refactored by Sourcery'

--- get-quote.py
+++ get-quote.py
@@ -2,9 +2,8 @@ import random
 def primary():
   print("Keep it logically awesome.")
 
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
+  with open("quotes.txt") as f:
+    quotes = f.readlines()
   last = len(quotes)-1
   rnd=random.randint(0,last)
   print(quotes[rnd])
